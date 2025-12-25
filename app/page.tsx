57404469f9e0bed5cfa94eba0de85f06fac9dded
elb00407 <Elb00407@g.bstu.by>
2025-12-25 19:01:26 +0300

    redirect home to register

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main style={{padding:40}}>
-      <h1>ToxicSkill</h1>
-      <p>Главная страница. Регистрация работает.</p>
-    </main>
-  );
+  redirect("/register");
 }
